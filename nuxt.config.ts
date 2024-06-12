7706a7a2a3d70f7cc549b6d62cf0916d6442ca35
dtczelo <dtczelo@gmail.com>
2024-06-12 21:53:39 +0200

    nuxt icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+    modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-icon"],
 
     tailwindcss: {
         cssPath: '~/assets/css/tailwind.css',
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
     <div>
         <h1 class="bg-red-600">Home</h1>
         <ThemeSwitcher />
+        <Icon name="uil:github" color="black" />
     </div>
 </template>
 
